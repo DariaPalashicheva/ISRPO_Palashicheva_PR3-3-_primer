cc51816e91a7912d23bac050d04fa63546adc71d
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-09-27 13:06:11 +1000

    код

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,61 @@ namespace ISRPO_Palashicheva_PR3_3__primer
         {
             InitializeComponent();
         }
+
+        private void Pusk_Click(object sender, RoutedEventArgs e)
+        {
+            //Получение исходных данных из TextBox
+            double x = Convert.ToDouble(X.Text);
+            double y = Convert.ToDouble(Y.Text);
+            double z = Convert.ToDouble(Z.Text);
+
+            //Ввод исходных данных в окно результатов
+            Itog.Text = "Результаты работы программы ст. Палашичевой" + Environment.NewLine;
+            Itog.Text += "При X = " + X.Text + Environment.NewLine;
+            Itog.Text += "При Y = " + Y.Text + Environment.NewLine;
+            Itog.Text += "При Z = " + Z.Text + Environment.NewLine;
+
+            //Определение номера выбранной функции
+            int n = 0;
+            double u;
+
+            if (sin.IsChecked == true) n = 1;
+            else if (exp.IsChecked == true) n = 2;
+            
+            //Вычисление U
+            switch (n)
+            {
+                case 0:
+                    if ((z - x) == 0) u = y * Math.Sin(x) * Math.Sin(x) + z;
+                    else if ((z - x) < 0) u = y * Math.Exp(Math.Sin(x)) - z;
+                    else u = y * Math.Sin(Math.Sin(x)) + z;
+                    Itog.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+                    break;
+                case 1:
+                    if ((z-x)==0) u = y * Math.Cos(x) * Math.Cos(x) + z;
+                    else if ((z-x)<0) u = y * Math.Exp(Math.Cos(x)) - z;
+                    else u = y * Math.Sin(Math.Cos(x)) + z;
+                    Itog.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+                    break;
+                case 2:
+                    if ((z - x)== 0) u = y * Math.Exp(x) * Math.Exp(x) + z;
+                    else if ((z - x)< 0) u = y * Math.Exp(Math.Exp(x)) - z;
+                    else u = y * Math.Sin(Math.Exp(x)) + z;
+                    Itog.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+                    break;
+                default:
+                    Itog.Text += "Решение не найдено " + Environment.NewLine;
+                    break;
+            }
+
+        }
+
+        private void Clin_Click(object sender, RoutedEventArgs e)
+        {
+            Itog.Clear();
+            X.Clear();
+            Y.Clear();
+            Z.Clear();
+        }
     }
 }
